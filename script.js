d7953e20a780ba6ce45cdb1a062219b5b6d39940
YOUR_USERNAME <YOUR_EMAIL>
2024-02-20 16:33:25 +0000

    refactored js code

--- script.js
+++ script.js
@@ -88,31 +88,117 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+// get input values for password generation
+var passLength = document.querySelector('#length');
+var lowercaseCharType = document.querySelector('#lowercase');
+var uppercaseCharType = document.querySelector('#uppercase');
+var numericCharType = document.querySelector('#numeric');
+var specialCharType = document.querySelector('#special');
+
 // Function to prompt user for password options
 function getPasswordOptions() {
+  // validation for password length
+  if (!passLength.value){
+    alert('Please enter a password length')
+  } else {
+    if (passLength.value < 8 || passLength.value > 128){
+      alert('Please enter a password length within the specified number of characters')
+    } else {
+      var len = passLength.value;
 
+ // validation for password char type
+ if (!lowercaseCharType.checked && !uppercaseCharType.checked && !numericCharType.checked && !specialCharType.checked){
+  alert('Please choose at least one character type to be included in the password')
+} else {
+  var checkedBoxes = [];
+  if (lowercaseCharType.checked) {
+    checkedBoxes.push('lowercase');
+  }
+  if (uppercaseCharType.checked) {
+    checkedBoxes.push('uppercase');
+  }
+  if (numericCharType.checked) {
+    checkedBoxes.push('numeric');
+  }
+  if (specialCharType.checked) {
+    checkedBoxes.push('special');
+  }
+  generatePassword(len, checkedBoxes);
+}
+}
+}
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+return arr[Math.floor(Math.random() * arr.length)];
 }
 
 // Function to generate password with user input
-function generatePassword() {
+function generatePassword(length, options) {
+var password = '';
+// Track the number of characters added
+var addedCharacters = 0;
 
+// Loop through the length of the password
+for (var i = 0; i < options.length; i++) {
+var type = options[i];
+
+// Determine the array for the current type
+var currentArray;
+if (type === 'numeric') {
+currentArray = numericCharacters;
+} else if (type === 'lowercase') {
+currentArray = lowerCasedCharacters;
+} else if (type === 'uppercase') {
+currentArray = upperCasedCharacters;
+} else if (type === 'special') {
+currentArray = specialCharacters;
+}
+
+// Add a random character from the current array
+password += getRandom(currentArray);
+
+// Increment the added characters count
+addedCharacters++;
+
+}
+
+// Fill the remaining characters to match the desired length
+while (addedCharacters < length) {
+  // Get a random character from any of the selected arrays
+  var randomType = options[Math.floor(Math.random() * options.length)];
+  var randomArray;
+  if (randomType === 'numeric') {
+    randomArray = numericCharacters;
+  } else if (randomType === 'lowercase') {
+    randomArray = lowerCasedCharacters;
+  } else if (randomType === 'uppercase') {
+    randomArray = upperCasedCharacters;
+  } else if (randomType === 'special') {
+    randomArray = specialCharacters;
+  }
+  
+  // Add a random character from the random array
+  password += getRandom(randomArray);
+  
+  // Increment the added characters count
+  addedCharacters++;
+}
+
+writePassword(password);
 }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+function writePassword(password) {
+// var password = generatePassword();
+var passwordText = document.querySelector('#password');
 
-  passwordText.value = password;
+passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+generateBtn.addEventListener('click', getPasswordOptions);
